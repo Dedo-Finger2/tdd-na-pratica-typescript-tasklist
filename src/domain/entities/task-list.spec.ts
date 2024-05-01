17dd1eb0f2ba3b6336e0048974d8056bfd13dce9
greg <antonioimportant@gmail.com>
2024-04-30 22:14:43 -0300

    feat: add TaskList instantiation tests

--- src/domain/entities/task-list.spec.ts
+++ src/domain/entities/task-list.spec.ts
@@ -1,12 +1,42 @@
-import { describe, it, expect } from "vitest";
 import { TaskList } from "./task-list";
+import { Task, TaskPriority } from "./task";
+
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import { set, reset } from "mockdate";
+
 
 describe("TaskList", () => {
+  beforeAll(() => {
+    set(new Date('2/20/2000'));  
+  });
+
+  afterAll(() => {
+    reset();
+  });
+
   it("should instanciate a new task list", () => {
     const taskList = new TaskList({
       createdAt: new Date(),
     });
 
     expect(taskList.createdAt).toEqual(new Date());
+  });
+
+  it("should be able to add tasks to a task list", () => {
+    const taskList = new TaskList({
+      createdAt: new Date(),
+    });
+
+    for (let index = 0; index < 10; index++) {
+      const newTask = new Task({
+        name: `Task ${index}`,
+        description: index <= 5 ? `Descrição ${index}` : undefined,
+        priority: index <= 5 ? TaskPriority.ALTA : TaskPriority.BAIXA,
+        dueDate: new Date(new Date().getTime() + index),
+      });
+      taskList.addTask(newTask);
+    };
+
+    expect(taskList.tasks.length).toEqual(10);
   });
 });
