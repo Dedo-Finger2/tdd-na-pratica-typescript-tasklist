738b878de48ba28cd32aa6cc952c9e3da4ddcf53
greg <antonioimportant@gmail.com>
2024-04-30 22:01:17 -0300

    feat: add TaskListEntity

---
+++ src/domain/entities/task-list.ts
@@ -0,0 +1,19 @@
+import { Task } from "vitest";
+
+export type TaskListParams = {
+  createdAt: Date;
+}
+
+export class TaskList {
+  public createdAt: Date;
+  public tasks: Task[];
+
+  constructor({ createdAt }: TaskListParams) {
+    this.createdAt = createdAt;
+    this.tasks = [];
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+}
