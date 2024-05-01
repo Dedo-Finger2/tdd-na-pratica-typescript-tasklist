226244bd9d0029b4f6de06d6bb8d9e08e3ba7f36
greg <antonioimportant@gmail.com>
2024-04-30 23:55:19 -0300

    feat: add build-get-task-by-name

--- src/factories/task.ts
+++ src/factories/task.ts
@@ -1,9 +1,32 @@
+import { TaskList } from "../domain/entities/task-list";
 import { TaskRepositorySpy } from "../repositories/spy/task";
 import { CreateTask } from "../use-cases/create-task";
+import { GetTaskByName } from "../use-cases/get-task-by-name";
 
 export const buildCreateTask = () => {
   const createTaskRepository = new TaskRepositorySpy();
   const sut = new CreateTask(createTaskRepository);
 
   return sut;
+}
+
+type GetTaskByNameReturn = { 
+  sut: GetTaskByName,
+  createTask: CreateTask,
+  taskList: TaskList,
+};
+
+export const buildGetTaskByName = (): GetTaskByNameReturn => {
+  const taskRepository = new TaskRepositorySpy();
+  const sut = new GetTaskByName(taskRepository);
+  const taskList = new TaskList({
+    createdAt: new Date(),
+  });
+  const createTask = new CreateTask(taskRepository);
+
+  return {
+    sut,
+    taskList,
+    createTask
+  };
 }
--- src/use-cases/get-task-by-name.spec.ts
+++ src/use-cases/get-task-by-name.spec.ts
@@ -1,18 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { Task, TaskPriority } from "../domain/entities/task";
-import { TaskRepositorySpy } from "../repositories/spy/task";
-import { GetTaskByName } from "./get-task-by-name";
-import { TaskList } from "../domain/entities/task-list";
-import { CreateTask } from "./create-task";
+import { buildGetTaskByName } from "../factories/task";
 
 describe("GetTaskByName", () => {
   it("should return the task with that provided name", async () => {
-    const taskRepository = new TaskRepositorySpy();
-    const taskList = new TaskList({
-      createdAt: new Date(),
-    });
-    const createTask = new CreateTask(taskRepository);
-    const sut = new GetTaskByName(taskRepository);
+    const { sut, taskList, createTask } = buildGetTaskByName();
     const taskName = "Task especial";
 
     const newTask = await createTask.execute({
